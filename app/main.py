058756a65e4c06c7ffd420085e1b02c7eccbd02a
Timothy Wikedzi <timothy.wikedzi@andela.com>
2020-09-10 12:34:01 +0300

    refactor main app add new routes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
-from flask import Flask 
-  
-app = Flask(__name__) 
-  
-@app.route("/") 
-def home_view(): 
-        return "<h1>Welcome to Geeks for Geeks</h1>"
+from flask import Flask, jsonify, request
+
+# initialize our Flask application
+app= Flask(__name__)
+@app.route("/name", methods=["POST"])
+def setName():
+    if request.method=='POST':
+        posted_data = request.get_json()
+        data = posted_data['data']
+        return jsonify(str("Successfully stored  " + str(data)))
+        
+@app.route("/message", methods=["GET"])
+def message():
+    posted_data = request.get_json()
+    name = posted_data['name']
+    return jsonify(" Hope you are having a good time " +  name + "!!!")
+
+#  main thread of execution to start the server
+if __name__=='__main__':
+    app.run(debug=True)
